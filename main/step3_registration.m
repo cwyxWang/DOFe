7f014ad7512bf6cc2de8069adc95b6f202bde99a
cwyxWang <70196239+cwyxWang@users.noreply.github.com>
2024-12-15 16:49:59 +0800

    Update step3_registration.m
    Debug

--- main/step3_registration.m
+++ main/step3_registration.m
@@ -1,7 +1,7 @@
 function step3_registration(config)
 %STEP3_REG 此处显示有关此函数的摘要
 %   此处显示详细说明
-    concat_d = round(config.zstep*1000/3/config.pixel_size);
+    concat_d = round(config.zstep/3/config.pixelsize);
        
     cache_path = fullfile(config.elastix_path, 'cache');
     if ~exist(cache_path, 'dir')
@@ -24,24 +24,24 @@ function step3_registration(config)
     stack = add_d(stack, concat_d);
     write_img(stack,result_name);
     d = d0+concat_d;
-    stitching_d = ceil(d0/2)+concat_d+1;
+    stitching_d = ceil(d0/2)+concat_d-5;
     for i = 2:data_num
-        temp = zeros(w,h,d);
-        corr = 0;
+        temp = zeros(h,w,d);
+        corr = 2;
         if mod(i,3)==1
             corr = 4;
         end
-        temp(:,:,d-d0+1-corr:d-corr) = read_stack(fullfile(config.save_path, data_name{i}),w,h,d0);
+        temp(:,:,d-d0+1-corr:d-corr) = read_stack(fullfile(config.save_path, data_name{i}),h,w,d0);
         write_img(temp,moving_name);
         system(['cd ',config.elastix_path, ' && elastix -f fixed.tif -m moving.tif -out cache -p p_3d.txt']);
-        new_stack = read_stack(moved_name,w,h,d);
+        new_stack = read_stack(moved_name,h,w,d);
         if mod(i,3)==0
             new_stack = (new_stack-120)*1.5+120;
         end
         
-        stack(:,:,d-stitching_d-1) = ((new_stack(:,:,d-stitching_d-1)+3*stack(:,:,d-stitching_d-1))/4-120)*1.3+120;
-        stack(:,:,d-stitching_d) = ((new_stack(:,:,d-stitching_d)+stack(:,:,d-stitching_d))/2-120)*1.3+120;
-        stack(:,:,d-stitching_d+1) = ((3*new_stack(:,:,d-stitching_d+1)+stack(:,:,d-stitching_d+1))/4-120)*1.3+120;
+         stack(:,:,d-stitching_d-1) = ((new_stack(:,:,d-stitching_d-1)+3*stack(:,:,d-stitching_d-1))/4-120)*1.3+120;
+         stack(:,:,d-stitching_d) = ((new_stack(:,:,d-stitching_d)+stack(:,:,d-stitching_d))/2-120)*1.3+120;
+         stack(:,:,d-stitching_d+1) = ((3*new_stack(:,:,d-stitching_d+1)+stack(:,:,d-stitching_d+1))/4-120)*1.3+120;
         %     stack(:,:,d-21) = (new_stack(:,:,d-21)+stack(:,:,d-21))/2;
         %     stack(:,:,d-22) = (new_stack(:,:,d-22)+stack(:,:,d-22))/2;
         
@@ -50,7 +50,7 @@ function step3_registration(config)
         write_img(stack,result_name);
         d = d+concat_d;
     end
-    write_img(stack(:,:,d-concat_d+1:d),fullfile(config.view_path,'result.tif'));
+    write_img(stack(:,:,1:d-concat_d),fullfile(config.view_path,'result.tif'));
 end
 
 %%
